ac560109959a16992d2dd3293e1b499e8498251b
Alan Matos <60579487+salanmato@users.noreply.github.com>
2023-09-02 19:04:08 -0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+//const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '66a13ffae9msh333d4c68170493bp165cd5jsn2739ae977944',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = "1c75c5c789bef01e5ca0b9a6f6ff1dc8"
+
